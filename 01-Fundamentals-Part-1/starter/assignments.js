c92c5002fbdc9d194c740ad2f95a8a990c3a6e3c
rickyaguayo <raguayo911@gmail.com>
2023-06-26 14:19:34 -0600

    new commits

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,5 +1,7 @@
 let country = "USA";
 let continent = "North America";
 let population = "331.9 million";
+let isIsland = false;
+let language;
 
-console.log(country, continent, population);
+console.log(country, population, isIsland, language);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,41 @@
 let js = "amazing";
+
+// switch/case
+
+let day = `sunday`;
+
+// switch (day) {
+//   case `monday`:
+//     console.log("it's monday!");
+//     break;
+//   case `tuesday`:
+//     console.log("it's tuesday!");
+//     break;
+//   case `wednesday`:
+//   case `thursday`:
+//     console.log("it's wed/thurs");
+//     break;
+//   case `friday`:
+//     console.log("almost the weekend baby");
+//     break;
+//   case `saturday`:
+//   case `sunday`:
+//     console.log("weekend baby!");
+//     break;
+//   default:
+//     console.log("not a valid day cuh");
+// }
+
+if (day === "monday") {
+  console.log("it's monday!");
+} else if (day === "tuesday") {
+  console.log("it's tuesday!");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("it's wed/thurs!");
+} else if (day === "friday") {
+  console.log("it's almost the weekend!");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("weekend baby!");
+} else {
+  console.log("not a valid day cuh");
+}

0714b97ba4219e85c857cdd1a740b3a2631e4962
rickyaguayo <raguayo911@gmail.com>
2023-08-02 22:15:04 -0600

    started guess the number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,45 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// console.log(document.querySelector('.guess').textContent.value);
+
+// let message = document.querySelector('.message').textContent;
+
+const number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = number;
+
+const guessFunc = function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Please enter number!';
+  } else if (guess === number) {
+    document.querySelector('.message').textContent = 'Correct!';
+  } else if (guess > number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game Over!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game Over!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+};
+
+document.querySelector('.check').addEventListener('click', guessFunc);

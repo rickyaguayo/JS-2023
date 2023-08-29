c6dbaa6aa410bc6b4eb7c510a78764035c224459
rickyaguayo <raguayo911@gmail.com>
2023-08-29 14:26:24 -0600

    section 9 up to challenge 1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -41,50 +41,58 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliverd to ${address} at ${time}`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`PASTA BRUH with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient, otherIngredients);
+  },
 };
 
 ////////DESTRUCTURING OBJECTS/////////
-const { name, openingHours, categories } = restaurant;
-// console.log(name, openingHours, categories);
+// const { name, openingHours, categories } = restaurant;
+// // console.log(name, openingHours, categories);
 
-//renaming variables with destructuring
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(restaurantName, hours, tags);
+// //renaming variables with destructuring
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantName, hours, tags);
 
-//setting default values
-const { menu = [], starterMenu: starters = [] } = restaurant;
-console.log(menu, starters);
+// //setting default values
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
 
-//mutating variables
-let a = 111;
-let b = 222;
-const obj = { a: 333, b: 444, c: 555 };
-({ a, b } = obj);
+// //mutating variables
+// let a = 111;
+// let b = 222;
+// const obj = { a: 333, b: 444, c: 555 };
+// ({ a, b } = obj);
 //must be wrapped in parantheses when redefining variables
 //js expects a code block if a line starts with curlies
-console.log(a, b);
+// console.log(a, b);
 
-//nested objects
-const {
-  fri: { open, close },
-} = hours;
-console.log(open, close);
+// //nested objects
+// const {
+//   fri: { open, close },
+// } = restaurant.openingHours;
+// console.log(open, close);
 
-//destructuring function arguments
-restaurant.orderDelivery({
-  time: '22:30',
-  address: '3517 Dresage Ct',
-  mainIndex: 2,
-  starterIndex: 3,
-});
+// //destructuring function arguments
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   address: '3517 Dresage Ct',
+//   mainIndex: 2,
+//   starterIndex: 3,
+// });
 
-restaurant.orderDelivery({
-  address: '3517 Dresage Ct',
-});
+// restaurant.orderDelivery({
+//   address: '3517 Dresage Ct',
+// });
 //if default is set, it is optional and doesnt need to be defined
 
 ////////DESTRUCTURING ARRAYS////////
@@ -122,3 +130,128 @@ restaurant.orderDelivery({
 
 // const [k = 1, l = 1, m = 1] = [8, 9];
 // console.log(k, l, m); //returns 8 9 1
+
+/////////SPREAD OPERATOR////////////
+// const arr = [1, 2, 3];
+// const newArr = [...arr, 4, 5];
+// console.log(newArr); //returns array
+// console.log(...newArr); //returns values from array
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// //copying an array
+// const mainMenuCopy = [...restaurant.mainMenu];
+// console.log(mainMenuCopy);
+
+// //merge multiple arrays
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+
+// //using spread operator in functions
+// const ingredients = [
+//   // prompt('Ingredient 1?'),
+//   // prompt('Ingredient 2?'),
+//   // prompt('Ingredient 3?'),
+// ];
+// console.log(ingredients);
+
+// restaurant.orderPasta(...ingredients);
+
+// //spread operator with objects
+// const newRestaurant = { foundedIn: 1989, ...restaurant, founder: 'Luca' };
+// console.log(newRestaurant);
+
+// const restaurantCopy = { ...restaurant };
+// restaurantCopy.name = 'Ristorante Roma';
+// console.log(restaurantCopy.name, restaurant.name);
+
+/////////REST//////////
+//spread operator will always be on the RIGHT of =
+const arr = [1, 2, ...[3, 4]];
+
+//rest operator will always be on the LEFT of =
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
+
+//can also use both at the same time
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+// console.log(pizza, risotto, otherFood);
+
+// //objects
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
+
+//functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+
+// add(2, 3);
+// add(2, 3, 4, 6, 2, 45);
+// add(2, 3, 7, 3, 2, 5, 7);
+
+// const x = [23, 5, 7];
+// add(...x);
+
+// restaurant.orderPizza('mushrooms', 'onions', 'pepperoni', 'jalapenos');
+// restaurant.orderPizza('pepperoni');
+
+////////SHORT CIRCUITING//////////
+
+//OR ||
+// console.log(null || undefined);
+// short circuiting with || can be used in place of ternary operators
+restaurant.numGuests = 0;
+// const guest1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guest1);
+
+// const guest2 = restaurant.numGuests || 10;
+// console.log(guest2);
+
+//AND &&
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza('pepperoni', 'mushrooms');
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza('pepperoni', 'mushrooms');
+
+// NULLISH COALESCING ??
+const guest3 = restaurant.numGuests ?? 10;
+// console.log(guest3);
+
+////////LOGICAL ASSIGNMENT OPERATORS//////////
+const rest1 = {
+  name: 'Las Salsas',
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'El Torro',
+  owner: 'Maria de la Bodega',
+};
+
+//with ||
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+//with ??
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+//with &&
+// rest1.owner = rest1.owner && 'Anonymous'; // this will add the owner property as undefined
+rest1.owner &&= 'Anonymous'; // this will not create owner property
+rest2.owner &&= 'Anonymous';
+
+// console.log(rest1);
+// console.log(rest2);
